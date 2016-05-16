fca7ab89db0c1c1ea91bc3e8b37c56fb617b91a0
Jacy Thompson <starbuck_4@hotmail.com>
2016-05-16 17:34:51 +0000

    Add layout and index view

--- site.rb
+++ site.rb
@@ -4,8 +4,6 @@ set :port, 4000
 set :bind, '0.0.0.0'
 get '/' do
   "I'm alive!"
-    "Welcome to my page!"
-    "I am a wife, a mom, an employee, a student, a runner and a wanna-be photographer."
 end
 get '/italian' do
   "Ciao!"
