d3ebd5b718fdb14519fdc6a49cca732dbc0a2bea
Jacy Thompson <starbuck_4@hotmail.com>
2016-05-16 17:22:07 +0000

    Fixing code.

--- site.rb
+++ site.rb
@@ -3,17 +3,16 @@ require 'sinatra'
 set :port, 4000
 set :bind, '0.0.0.0'
 get '/' do
-  <h1>"I'm alive!"</h1>
-    <h2>"Welcome to my page!"</h2>
-    <p>"I am a wife, a mom, an employee, a student, a runner and a photographer."</p>
-
+  <h1> "I'm alive!" </h1>
+    <h2> "Welcome to my page!" </h2>
+    <p> "I am a wife, a mom, an employee, a student, a runner and a wanna-be photographer." </p>
 end
 get '/italian' do
-  <h1>"Ciao!"</h1>
+  <h1> "Ciao!" </h1>
 end
 get '/spanish' do
-  <h1>"Hola!"</h1>
+  <h1> "Hola!" </h1>
 end
 get '/chinese' do
-  <h1>"Ni hao!"</h1>
-  end
+  <h1> "Ni hao!" </h1>
+end
