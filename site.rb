73a51b5f436ad377682a895ea7a09583947238ec
Jacy Thompson <starbuck_4@hotmail.com>
2016-05-16 17:19:36 +0000

    Added new routes for Spanish and Chinese

--- site.rb
+++ site.rb
@@ -3,8 +3,17 @@ require 'sinatra'
 set :port, 4000
 set :bind, '0.0.0.0'
 get '/' do
-  "I'm alive!"
+  <h1>"I'm alive!"</h1>
+    <h2>"Welcome to my page!"</h2>
+    <p>"I am a wife, a mom, an employee, a student, a runner and a photographer."</p>
+
 end
 get '/italian' do
-  "Ciao!"
+  <h1>"Ciao!"</h1>
 end
+get '/spanish' do
+  <h1>"Hola!"</h1>
+end
+get '/chinese' do
+  <h1>"Ni hao!"</h1>
+  end
